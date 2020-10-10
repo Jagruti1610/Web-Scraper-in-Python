b020087e6171226abbd3d369ac78b55f430c7d8c
Jagruti1610 <tiwarijagruti1@gmail.com>
2020-10-10 17:51:53 +0530

    Update web-scraper.py
    Removed unwanted imports

--- web-scraper.py
+++ web-scraper.py
@@ -1,11 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.common import exceptions  
-from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
 import pandas as pd
 
